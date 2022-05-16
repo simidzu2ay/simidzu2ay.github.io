5cc5c8e3369866b53885489ceed3cfc8590f81ce
Simidzu Ay <simidzu2ay@protonmail.com>
2022-05-16 20:38:45 +0000

    fix moved icona on md screen

--- src/components/MainInfo.svelte
+++ src/components/MainInfo.svelte
@@ -15,11 +15,11 @@
     px-4 sm:scale-[0.85] md:scale-[0.9] md:flex-row md:px-8 lg:scale-100 xl:scale-[1.1]"
 >
   <section
-    class="space-x-2 space-y-2 flex flex-col justify-center items-center"
+    class="flex flex-col items-center justify-center space-x-2 space-y-2"
   >
     <img src={imgSrc} alt="Avatar" class="h-32 w-32 rounded-full" />
 
-    <p class="cursor-default text-lg text-[#e2e2e2] flex">
+    <p class="flex cursor-default text-lg text-[#e2e2e2]">
       <Svg svg={quotes.left} fill="#e2e2e2" className="quote -mt-1" />
       <span class="px-2">Только дайте остаться собой...</span>
       <Svg svg={quotes.right} fill="#e2e2e2" className="quote mt-2" />
@@ -27,9 +27,7 @@
   </section>
 
   <div id="links" class="flex flex-col items-center justify-center space-y-4">
-    <section
-      class="mx-16 mt-3 flex items-center justify-center gap-4 md:mx-0 md:ml-4"
-    >
+    <section class="mx-16 mt-3 flex items-center justify-center gap-4 md:mx-4">
       {#each links as link}
         <a href={link.href} target="_blank">
           <Svg
--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,7 @@
 import { minify } from 'html-minifier';
 import { prerendering } from '$app/env';
 import type { Handle } from '@sveltejs/kit';
- 
+
 const minification_options = {
   collapseBooleanAttributes: true,
   collapseWhitespace: true,
@@ -16,18 +16,18 @@ const minification_options = {
   removeOptionalTags: true,
   removeRedundantAttributes: true,
   removeScriptTypeAttributes: true,
-  removeStyleLinkTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true
 };
- 
+
 export const handle: Handle = async ({ event, resolve }) => {
   const response = await resolve(event);
- 
+
   if (prerendering && response.headers.get('content-type') === 'text/html') {
     return new Response(minify(await response.text(), minification_options), {
       status: response.status,
       headers: response.headers
     });
   }
- 
+
   return response;
-}
+};
